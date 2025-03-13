20e3443b4f601def12b65a98208bea1e1f8bf9ea
Nikki Keister <nicolerkeister@gmail.com>
2025-03-12 23:30:01 -0400

    rudimentary homepage intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import text from './utils/text';
 import './globals.css';
+import './homepage.css';
 
 export default function Home() {
   return (
-    <div>
-      <div>{text.homepage.jobTitle}</div>
-      <div>{text.homepage.intro}</div>
+    <div className='homebio'>
+        <div className='job-title'>{text.homepage.jobTitle}</div>
+        <div className='intro-button-container'>
+          <div className='intro'>{text.homepage.intro}</div>
+          <button>Copy My Email</button>
+        </div>
+        <div className='locations'>
+          Locations:
+          <span>Seattle, WA</span>
+          <span>Brooklyn, NY</span>
+          <span>Remote</span>
+        </div>
     </div>
   );
 };
